71391a06469784c8fad76fd8a6d735e64fa9d0fa
vkt823 <vkt823@alumni.ku.dk>
2023-03-13 10:57:22 +0100

    update

--- Lectures/Lec 5/5 - Structure/solve.py
+++ Lectures/Lec 5/5 - Structure/solve.py
@@ -2,4 +2,9 @@
 def all(model):
 
     model.y = 2*model.x
+    #CHR: Inside the function, a new variable "y" is 
+    # defined in the "model" object as twice the value 
+    # of "x" attribute of "model". Then, a string is 
+    # printed using an f-string that includes the value 
+    # of "model.x".
     print(f'solving - with {model.x = :}')
